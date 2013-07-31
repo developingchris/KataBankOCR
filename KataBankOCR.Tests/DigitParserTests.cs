4b6a194bd6374bde913ee52419ad52ce7d48d5b3
Chris Chandler <chris@developingchris.com>
2013-07-30 23:14:17 -0400

    first test pass, and infrastructure in place, time to drive

---
+++ KataBankOCR.Tests/DigitParserTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace KataBankOCR.Tests
+{
+    [TestFixture]
+    public class DigitParserTests
+    {
+        [Test]
+        public void CountLines()
+        {
+            var file = @"
+ _  _  _  _  _  _  _  _  _ 
+| || || || || || || || || |
+|_||_||_||_||_||_||_||_||_|
+                                                      
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |
+                            _  _  _  _  _  _  _  _  _ 
+ _| _| _| _| _| _| _| _| _|
+|_ |_ |_ |_ |_ |_ |_ |_ |_ 
+                           "
+
+
+        }
+
+    }
+}
