02ac110ac8b796d6aa7cbf048fe7d53919978d35
Chris Chandler <chris@developingchris.com>
2013-07-31 01:26:56 -0400

    commenting out test that actually are not dry to the cause, they were important for getting digits parsing but are actually maintenance overhead in the long run

--- KataBankOCR.Tests/DigitParserTests.cs
+++ KataBankOCR.Tests/DigitParserTests.cs
@@ -59,67 +59,67 @@ namespace KataBankOCR.Tests
 
         }
 
-        [Test]
-        public void ZeroLine()
-        {
-            var parser = new LineParser(File);
-            parser.Parse();
-            Assert.AreEqual(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, parser.Lines[0]);
-        }
+        //[Test]
+        //public void ZeroLine()
+        //{
+        //    var parser = new LineParser(File);
+        //    parser.Parse();
+        //    Assert.AreEqual(new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, parser.Lines[0]);
+        //}
 
-        [Test]
-        public void OnesLine()
-        {
-            var parser = new LineParser(File);
-            parser.Parse();
-            Assert.AreEqual(new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1 }, parser.Lines[1]);
-        }
+        //[Test]
+        //public void OnesLine()
+        //{
+        //    var parser = new LineParser(File);
+        //    parser.Parse();
+        //    Assert.AreEqual(new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1 }, parser.Lines[1]);
+        //}
 
-        [Test]
-        public void TwosLine()
-        {
-            var parser = new LineParser(File);
-            parser.Parse();
-            Assert.AreEqual(new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, parser.Lines[2]);
-        }
-        [Test]
-        public void ThreesLine()
-        {
-            var parser = new LineParser(File);
-            parser.Parse();
-            Assert.AreEqual(new List<int> { 3, 3, 3, 3, 3, 3, 3, 3, 3 }, parser.Lines[3]);
-        }
-        [Test]
-        public void FoursLine()
-        {
-            var parser = new LineParser(File);
-            parser.Parse();
-            Assert.AreEqual(new List<int> { 4, 4, 4, 4, 4, 4, 4, 4, 4 }, parser.Lines[4]);
-        }
+        //[Test]
+        //public void TwosLine()
+        //{
+        //    var parser = new LineParser(File);
+        //    parser.Parse();
+        //    Assert.AreEqual(new List<int> { 2, 2, 2, 2, 2, 2, 2, 2, 2 }, parser.Lines[2]);
+        //}
+        //[Test]
+        //public void ThreesLine()
+        //{
+        //    var parser = new LineParser(File);
+        //    parser.Parse();
+        //    Assert.AreEqual(new List<int> { 3, 3, 3, 3, 3, 3, 3, 3, 3 }, parser.Lines[3]);
+        //}
+        //[Test]
+        //public void FoursLine()
+        //{
+        //    var parser = new LineParser(File);
+        //    parser.Parse();
+        //    Assert.AreEqual(new List<int> { 4, 4, 4, 4, 4, 4, 4, 4, 4 }, parser.Lines[4]);
+        //}
 
-        [Test]
-        public void FivesLine()
-        {
-            var parser = new LineParser(File);
-            parser.Parse();
-            Assert.AreEqual(new List<int> { 5, 5, 5, 5, 5, 5, 5, 5, 5 }, parser.Lines[5]);
-        }
+        //[Test]
+        //public void FivesLine()
+        //{
+        //    var parser = new LineParser(File);
+        //    parser.Parse();
+        //    Assert.AreEqual(new List<int> { 5, 5, 5, 5, 5, 5, 5, 5, 5 }, parser.Lines[5]);
+        //}
 
-        [Test]
-        public void SixLine()
-        {
-            var parser = new LineParser(File);
-            parser.Parse();
-            Assert.AreEqual(new List<int> { 6, 6, 6, 6, 6, 6, 6, 6, 6 }, parser.Lines[6]);
-        }
+        //[Test]
+        //public void SixLine()
+        //{
+        //    var parser = new LineParser(File);
+        //    parser.Parse();
+        //    Assert.AreEqual(new List<int> { 6, 6, 6, 6, 6, 6, 6, 6, 6 }, parser.Lines[6]);
+        //}
 
-        [Test]
-        public void AllDigits()
-        {
-            var parser = new LineParser(File);
-            parser.Parse();
-            Assert.AreEqual(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, parser.Lines.Last());
-        }
+        //[Test]
+        //public void AllDigits()
+        //{
+        //    var parser = new LineParser(File);
+        //    parser.Parse();
+        //    Assert.AreEqual(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, parser.Lines.Last());
+        //}
 
 
         [Test]
